e118928b624966ca7cae0d3b23f7922cd092867f
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-08-30 23:34:48 +0530

    ud

--- src/demo.java
+++ src/demo.java
@@ -9,33 +9,21 @@ public class demo {
         System.out.println(ans);
     }
         static int lenOfLongSubarr(int[] arr, int n, int k) {
-            // HashMap to store (sum, index) pairs
             HashMap<Integer, Integer> map = new HashMap<>();
             int sum = 0, maxLen = 0;
 
-            // Traverse the given array
             for (int i = 0; i < n; i++) {
-                // Accumulate sum
                 sum += arr[i];
-
-                // When subarray starts from index 0
                 if (sum == k) {
                     maxLen = i + 1;
                 }
-
-                // If 'sum' is not present in the map, add it with the index 'i'
                 if (!map.containsKey(sum)) {
                     map.put(sum, i);
                 }
-
-                // Check if 'sum - k' is present in the map or not
                 if (map.containsKey(sum - k)) {
-                    // Update maxLength if needed
                     maxLen = Math.max(maxLen, i - map.get(sum - k));
                 }
             }
-
-            // Return the required maximum length
             return maxLen;
         }
     }
