376660775de8b198e62b3d9f85f61056768bf6f9
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-09-02 23:53:01 +0530

    mk

--- src/demo.java
+++ src/demo.java
@@ -16,6 +16,14 @@ public class demo {
                 sum += arr[i];
                 if (sum == k) {
                     maxLen = i + 1;
-
+                }
+                if (!map.containsKey(sum)) {
+                    map.put(sum, i);
+                }
+                if (map.containsKey(sum - k)) {
+                    maxLen = Math.max(maxLen, i - map.get(sum - k));
+                }
+            }
+            return maxLen;
         }
     }
