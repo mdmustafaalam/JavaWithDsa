aca9c9194c7282ac75d3d2bf52788721c7588a66
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-09-04 23:10:07 +0530

    lk

--- src/demo.java
+++ src/demo.java
@@ -12,6 +12,18 @@ public class demo {
             HashMap<Integer, Integer> map = new HashMap<>();
             int sum = 0, maxLen = 0;
 
-
+            for (int i = 0; i < n; i++) {
+                sum += arr[i];
+                if (sum == k) {
+                    maxLen = i + 1;
+                }
+                if (!map.containsKey(sum)) {
+                    map.put(sum, i);
+                }
+                if (map.containsKey(sum - k)) {
+                    maxLen = Math.max(maxLen, i - map.get(sum - k));
+                }
+            }
+            return maxLen;
         }
     }
