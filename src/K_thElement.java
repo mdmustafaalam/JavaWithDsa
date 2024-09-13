0bcf839b4f0d42580c74c8d6a406f0ae15925a0a
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-09-13 11:32:49 +0530

    ,l

--- src/K_thElement.java
+++ src/K_thElement.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class k_thElement {
+public class K_thElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
---
+++ src/RearrageArray.java
@@ -0,0 +1,23 @@
+class RearrageArray {
+    public static void main(String arr[]){
+
+        int[] arr1 = {1,2,3,4,5,6};
+
+        int result = rearrange(arr1);
+        System.out.println(result);
+    }
+
+    static int rearrange(int[] arr1) {
+        int max = arr1[0];
+        int min = arr1[0];
+        for (int i = 1; i < arr1.length; i++) {
+            if (arr1[i] > max)
+                max = arr1[i];
+        }
+        for (int j = 1; j < arr1.length; j++) {
+            if (arr1[j] < min)
+                min = arr1[j];
+        }
+        return (max, min);
+    }
+}
