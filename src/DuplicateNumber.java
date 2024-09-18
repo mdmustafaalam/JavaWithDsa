076214ea0f2966831bdd9ca173ee7745b9f7305d
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-09-18 22:02:50 +0530

    ml

--- src/DuplicateNumber.java
+++ src/DuplicateNumber.java
@@ -6,5 +6,16 @@ public class DuplicateNumber {
         System.out.println(result);
     }
 
-
+    static int duplicateNumber(int[] arr1) {
+        if (arr1.length >= 0) {
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = i + 1; j < arr1.length; j++) {
+                    if (arr1[i] == arr1[j]) {
+                        System.out.print(arr1[j] + " ");
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 }
