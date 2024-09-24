c42e8a5e31d15e2c513367c8dbc107181ce5bad8
MD Mustafa Alam <mustafaalam3240@gmail.com>
2024-09-24 20:22:36 +0530

    mk

--- src/ExceptionallyOdd.java
+++ src/ExceptionallyOdd.java
@@ -7,13 +7,5 @@ public class ExceptionallyOdd {
         int ans = getOddOccurrence(arr);
         System.out.println(ans);
     }
-    static int getOddOccurrence(int[] arr) {
-        // code here
-        int res = 0;
-        for (int i = 0; i < arr.length; i++) {
-            res = res ^ arr[i];
-        }
-        return res;
 
-    }
 }
